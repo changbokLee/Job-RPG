44c0a60cbb03b109791442099c7d1c5f8ffffe96
changbokLee <changbok2453@gmail.com>
2020-06-08 20:22:09 +0900

    공부중

---
+++ GithubCopy/js/main.js
@@ -0,0 +1,30 @@
+(function(window ,document){
+    'use strict';
+
+    const $toggles = document.querySelectorAll('.toggle'); //N
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function(){
+        toggleElements();
+    });
+
+    window.addEventListener('resize' ,function(){
+        if (window.innerWidth >1024){
+            offElements();
+        }
+        console.log('Resized');
+    }) ;
+
+    function toggleElements(){
+        [].forEach.call($toggles, function(toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+
+    function offElements(){
+        [].forEach.call($toggles, function(toggle) {
+            toggle.classList.remove('on');
+        });
+
+    }
+})(window, document)
